ebe5cab4de9999f3f62c919f9237b8900ee19fa2
Oscar_YA_Zhang <oscar.ya.zhang@outlook.com>
2021-03-20 15:42:16 +0800

    Update CircleFormation.h header

--- include/CircleFormation.h
+++ include/CircleFormation.h
@@ -6,7 +6,6 @@
 #include <stdio.h>               // This circleFormation.c example uses printf/fflush
 #include "codegenReal2Mission.h" // Model's header file
 #include "rtwtypes.h"
-#include "zero_crossing_types.h"
 #include <vector>
 #include "builtin_typeid_types.h"
 #include "multiword_types.h"
@@ -14,13 +13,13 @@
 #include "rt_logging.h"
 #include "MissionData.h"
 #ifndef SAVEFILE
-#define MATFILE2(file) #file ".mat"
-#define MATFILE1(file) MATFILE2(file)
-#define MATFILE MATFILE1(MODEL)
+#define MATFILE2(file)                 #file ".mat"
+#define MATFILE1(file)                 MATFILE2(file)
+#define MATFILE                        MATFILE1(MODEL)
 #else
-#define QUOTE1(name) #name
-#define QUOTE(name) QUOTE1(name) // need to expand name
-#define MATFILE QUOTE(SAVEFILE)
+#define QUOTE1(name)                   #name
+#define QUOTE(name)                    QUOTE1(name)              // need to expand name 
+#define MATFILE                        QUOTE(SAVEFILE)
 #endif
 
 class CircleFormation
