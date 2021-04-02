72c142aa22ed0dcb11ac67306d8f7ceb70ddd8f9
Oscar_YA_Zhang <oscar.ya.zhang@outlook.com>
2021-04-02 20:49:04 +0800

    change mutex logic

--- MissionApp/src/MissionData.cpp
+++ MissionApp/src/MissionData.cpp
@@ -9,15 +9,13 @@ MissionData::~MissionData()
 
 // call by algorithm
 void MissionData::setExtY(const codegenReal2MissionModelClass::ExtY_codegenReal2Mission_T& algExtY) {
-	mutexExtY.lock();
+  const std::lock_guard<std::mutex> lock(mutexExtY);
 	this->ExtY = algExtY;
-	mutexExtY.unlock();
 }
 
 const codegenReal2MissionModelClass::ExtU_codegenReal2Mission_T& MissionData::getExtU() {
-	mutexExtU.lock();
+	const std::lock_guard<std::mutex> lock(mutexExtU);
 	return this->ExtU;
-	mutexExtU.unlock();
 }
 
 // call by IPC
@@ -30,23 +28,21 @@ void MissionData::setCtrlCmd(int8_t cmdId)
 //--------------供数据收发软件更新位置-------------------//
 void MissionData::updatePos(FlightStatus fs)
 {
-  mutexExtU.lock();
+  const std::lock_guard<std::mutex> lock(mutexExtU);
   this->ExtU.RealUAVLatLonState = {fs.lat, fs.lon, fs.height, fs.airspeed, fs.yaw, fs.pitch, fs.roll};
   this->ExtU.FlightMode = fs.flightMode;
   this->ExtU.GroundSpd = fs.groundspeed;
   this->ExtU.VectorSpd = {fs.eastSpeed, fs.northSpeed, fs.skySpeed};
   this->ExtU.Altitude = fs.alt;
-  mutexExtU.unlock();
 }
 
 //--------------供数据收发软件获取期望航点、速度---------//
 void MissionData::getExpectedPos(std::vector<double> &expPos)
 {
-  mutexExtY.lock();
+  const std::lock_guard<std::mutex> lock(mutexExtY);
   expPos.push_back(this->ExtY.RefAirspeed);
   expPos.push_back(this->ExtY.LookAheadPoint_j.Longitude_deg);
   expPos.push_back(this->ExtY.LookAheadPoint_j.Latitude_deg);
   expPos.push_back(this->ExtY.LookAheadPoint_j.Height_meter);
-  mutexExtY.unlock();
 }
 
