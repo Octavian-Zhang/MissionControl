2b8733633437abc6e736f48100bb1f3ff17df8e4
Oscar_YA_Zhang <oscar.ya.zhang@outlook.com>
2021-05-07 16:39:55 +0800

    debug hdg angle mismatch error

--- MissionApp/src/CircleFormation.cpp
+++ MissionApp/src/CircleFormation.cpp
@@ -39,7 +39,6 @@ void CircleFormation::codegenReal2MissionModelClassRecvData_IndividualUAVCmdT::
 	// Add receive data logic here
 	//下一行代码获取数据收发软件发送过来的任务指令
 	*data = *missionCmd;
-	std::cout << data->SequenceId << std::endl;
 }
 
 void CircleFormation::renameMATfile(void)
